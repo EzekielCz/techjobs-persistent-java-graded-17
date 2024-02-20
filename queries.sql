9ec871739e23939d2be548fbd5a47ac38a8f721f
EzekielCz <ezechielcazacu@gmail.com>
2024-02-19 19:17:52 -0600

    dumb thing

--- queries.sql
+++ queries.sql
@@ -1,7 +1,16 @@
 --Part 1
+Id: INTEGER,
+name: VARCHAR(67),
+employer: VARCHAR(67),
+skills: VARCHAR(255)
 
 --Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
+DROP TABLE job;
 
 --Part 4
+SELECT * FROM skill INNER JOIN job_skills ON skill.id = job_skills.skills_idWHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
